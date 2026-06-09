98ee5ab7051045c2e3e7100f463a783020c6e05e
Roshan Biswal <roshanbiswal9439@gmail.com>
2026-06-09 19:11:58 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1222
+let accountEmail = "roshan@gmail.com"
+var accountPassword = "1234"
+let accountState;
+accountCity = "Raipur"
+
+accountEmail = "rb@gmail.com"
+accountPassword = "21313"
+accountCity = "bhilai"
+
+
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as new version
+
+//  alert(3+3)  //we are using node js not browser
+
+console.log(3+1)
+//code readability is should be high
+console.log("roshan")
+
+
+let name = "Roshan"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to pwr 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
