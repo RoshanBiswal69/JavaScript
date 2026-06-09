8a84f91d41b5c6ecdb9df5e2075db3a81b3fac43
Roshan Biswal <roshanbiswal9439@gmail.com>
2026-06-09 19:40:32 +0000

    conversion operation 2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,42 @@ let someNumber = 33
 
 let stringnumber = String(someNumber);
 console.log(stringnumber);
+
+
+
+// *************** operation *************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2/2)
+
+
+let str1 = "hello"
+let str2 = " roshan"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+
+console.log("1" + 2)
+console.log(1+ "2")
+console.log(1+ "2" + 2)
+console.log(1+2+ "2")
+
+
+console.log(+true)
+let num1, num2, num3
+
+num1 = num2 = mu3 = 2+2
+
+let gamecounter = 100
+gamecounter++
+++gamecounter
+console.log(gamecounter)
